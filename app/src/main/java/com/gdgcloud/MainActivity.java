c8823939f0d31c440c1dbaf0d7810792a3a429b5
Chit1998 <ssshashikant349@gmail.com>
2022-09-06 13:26:27 +0530

    firebase authentication done

--- app/src/main/java/com/gdgcloud/MainActivity.java
+++ app/src/main/java/com/gdgcloud/MainActivity.java
@@ -2,17 +2,29 @@ package com.gdgcloud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.FrameLayout;
 
 import com.gdgcloud.fragment.ScannerFragment;
+import com.gdgcloud.preferences.UserPreferences;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     FrameLayout framelayout;
+
+    private UserPreferences preferences;
+    private HashMap<String, String> userData;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        preferences = new UserPreferences(this);
+        userData = preferences.userData();
 
         framelayout = findViewById(R.id.framelayout);
 
@@ -20,5 +32,17 @@ public class MainActivity extends AppCompatActivity {
                 .beginTransaction()
                 .replace(framelayout.getId(), new ScannerFragment())
                 .commit();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (userData.get(UserPreferences.IS_EMAIL_ID) == null){
+            startActivity(new Intent(getApplicationContext(), SignInActivity.class));
+            finish();
+        }else {
+            Log.d("TAG", "onStart: Login");
+        }
+
     }
 }
--- app/src/main/java/com/gdgcloud/SignInActivity.java
+++ app/src/main/java/com/gdgcloud/SignInActivity.java
@@ -1,16 +1,31 @@
 package com.gdgcloud;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import com.gdgcloud.fragment.ScannerFragment;
+
+import com.gdgcloud.preferences.UserPreferences;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.HashMap;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private final String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    private EditText eEmailAddress, ePassword;
+    private UserPreferences preferences;
+    private HashMap<String, String> userData;
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +35,50 @@ public class SignInActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         getWindow().setStatusBarColor(Color.TRANSPARENT);
         setContentView(R.layout.activity_sign_in);
+        auth = FirebaseAuth.getInstance();
+        preferences = new UserPreferences(this);
+        userData = preferences.userData();
+
+        eEmailAddress = findViewById(R.id.eEmailAddress);
+        ePassword = findViewById(R.id.ePassword);
+
     }
 
     public void LogIn(View view) {
-        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        if (eEmailAddress.getText().toString().isEmpty()){
+            eEmailAddress.setError("Empty");
+        }else if (!eEmailAddress.getText().toString().matches(emailPattern)){
+            eEmailAddress.setError("Please check your mail-id");
+        }else if (ePassword.getText().toString().isEmpty()){
+            ePassword.setError("Empty");
+        }else {
+            auth.signInWithEmailAndPassword(
+                    eEmailAddress.getText().toString(),
+                    ePassword.getText().toString())
+                            .addOnCompleteListener(task -> {
+                                if (task.isSuccessful()){
+                                    preferences.SignIn(eEmailAddress.getText().toString(),ePassword.getText().toString());
+                                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                                    finish();
+                                }else {
+                                    Toast.makeText(SignInActivity.this, "Please check your mail-id and password..!", Toast.LENGTH_SHORT).show();
+                                }
+                            }).addOnFailureListener(e -> {
+                                Toast.makeText(SignInActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                Log.d("TAG", "onFailure: "+e.getMessage());
+                            });
+
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (userData.get(UserPreferences.IS_EMAIL_ID) != null){
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }else {
+            Log.d("TAG", "onStart: Logout");
+        }
     }
 }
